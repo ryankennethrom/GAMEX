d5a949a235096bc54c29eeaa2463b55e3cd5f9a3
ryan <rom@ualberta.ca>
2024-08-24 00:16:27 -0600

    fix bug where the player capsule falls on its side

---
+++ GAMEX/Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    public Rigidbody rb;
+    public bool cubeIsOnTheGround = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump") && cubeIsOnTheGround){
+            rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+            cubeIsOnTheGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.tag == "Ground") {
+            cubeIsOnTheGround = true;
+        }
+    }
+}
